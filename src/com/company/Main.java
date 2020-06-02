9ac7d397c8f34f2e5aa5fa5505a1c2aea3f6a82b
BingNa Yang <bingnayang@gmail.com>
2020-06-02 16:22:09 -0400

    fix option looping problem

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,16 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
+
     private static TheSystem apptSystem = new TheSystem();
     private static final DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         Date createdDate = new Date();
         LocalDate appointmentDate = LocalDate.of(2020,6,14);
         LocalTime appointmentTime = LocalTime.of(5,30);
@@ -23,21 +27,22 @@ public class Main {
         // Print starting message and print menu options
         startMessage();
         menuOptions();
-
-
+        int option;
+        boolean quit = false;
         Appointment newAppointment1 = Appointment.bookAppointment("John","Sam York","213-324-5566",dateFormat.format(createdDate), appointmentDate, appointmentTime,"arrived",4);
         Appointment newAppointment2 = Appointment.bookAppointment("John","Winsley Horan","213-789-3454",dateFormat.format(createdDate), appointmentDate1, appointmentTime1,"cancel",5);
         Appointment newAppointment3 = Appointment.bookAppointment("Tim","Danny Oska","271-342-4432",dateFormat.format(createdDate), appointmentDate, appointmentTime,"scheduled",5);
         Appointment newAppointment4 = Appointment.bookAppointment("Elina","Joy Nathon","271-342-3423",dateFormat.format(createdDate), appointmentDate, appointmentTime,"scheduled",8);
-        apptSystem.addNewAppointment(newAppointment1);
-        apptSystem.addNewAppointment(newAppointment2);
-        apptSystem.addNewAppointment(newAppointment3);
-        apptSystem.addNewAppointment(newAppointment4);
-        apptSystem.deleteAppointment(newAppointment1);
-        apptSystem.printAppointmentList();
+//        apptSystem.addNewAppointment(newAppointment1);
+//        apptSystem.addNewAppointment(newAppointment2);
+//        apptSystem.addNewAppointment(newAppointment3);
+//        apptSystem.addNewAppointment(newAppointment4);
+//        apptSystem.deleteAppointment(newAppointment1);
+//        apptSystem.printAppointmentList();
 
-        while(true){
-            int option = 1;
+        while(!quit){
+            System.out.println("Enter your option: ");
+             option = scanner.nextInt();
             switch (option){
                 case 1:
                     // Add new appointment
@@ -55,6 +60,8 @@ public class Main {
                     // Print out the appointment list
                     System.out.println("Print out all Appointment");
                     break;
+                case 5:
+                    quit = true;
                 default:
                     break;
             }
